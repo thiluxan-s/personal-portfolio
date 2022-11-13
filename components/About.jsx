8541e0f433ffa896896e69a1de088537eb12fdd3
thiluxan <thiluxan.s@gmail.com>
2022-11-13 10:08:01 -0500

    first commit of portfolio website

---
+++ components/About.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div id='about' className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">About</p>
+          <h2>Who I Am</h2>
+          <p className="py-2 text-gray-600">// I am not your normal developer.</p>
+          <p className="py-2 text-gray-600">
+            I have spent the last 14 years in the hospitality service industry,
+            managing a chain of restaurants and cafes. I have always had a knack
+            for technology and working with computers. In 2014 I started working
+            in SEO and content management. This led to me picking up HTML & CSS
+            to make minor edits to websites of various businesses. What I
+            thought was just a few edits quickly turned into a love for
+            programming.
+          </p>
+          <p className="py-2 text-gray-600">
+            Fascinated with how intricate programming can be I was quickly drawn
+            to learn more. I started learning JavaScript and was even more
+            enthused with making websites interactive. I then started
+            freelancing for various contractees. I am now spending my time
+            building projects with NextJS while learning new technologies.
+          </p>
+          <p className="py-2 text-gray-600 underline cursor-pointer">Check out some of my latest projects.</p>
+        </div>
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <img className="rounded-xl"
+            src="https://images.unsplash.com/photo-1553390774-b4822481c894?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=744&q=80"
+            alt="/"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ components/Projects.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import photographySite from "../public/assets/projects/Vizion.png";
+import cryptoCoins from "../public/assets/projects/CryptoCoinSearch.png"
+import cookBook from "../public/assets/projects/cookbook.png"
+import dexApp from "../public/assets/projects/dex.png"
+import ProjectItem from "./ProjectItem";
+
+const Projects = () => {
+  return (
+    <div id='projects' className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">What I've Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="Photography Services"
+            backgroundImg={photographySite}
+            projectUrl="/photography"
+            techStack="NextJS"
+          />
+          <ProjectItem
+            title="Crypto App"
+            backgroundImg={cryptoCoins}
+            projectUrl="/crypto"
+            techStack="React JS"
+          />
+          <ProjectItem
+            title="Cookbook"
+            backgroundImg={cookBook}
+            projectUrl="/cookbook"
+            techStack="React JS | Firebase"
+          />
+          <ProjectItem
+            title="Crypto DEX (1inch API)"
+            backgroundImg={dexApp}
+            projectUrl="/dex"
+            techStack="JavaScript"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
