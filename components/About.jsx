d82f4386f54997e39d9c205ce9b719faacf0e781
thiluxan <thiluxan.s@gmail.com>
2023-04-24 15:01:44 -0400

    disabled an eslint feature to About and Contact components in order to get image imports working correctly

--- components/About.jsx
+++ components/About.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from "react";
 
--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,14 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Link from "next/link";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import { HiOutlineChevronDoubleUp } from "react-icons/hi"
-
+import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 
 const Contact = () => {
   return (
-    <div id='contact' className="w-full lg:h-screen">
+    <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Contact
@@ -63,7 +63,9 @@ const Contact = () => {
                     />
                   </div>
                   <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">Phone Number</label>
+                    <label className="uppercase text-sm py-2">
+                      Phone Number
+                    </label>
                     <input
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
@@ -71,34 +73,39 @@ const Contact = () => {
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
-                    <label className="uppercase text-sm py-2">Email</label>
-                    <input
-                      className="border-2 rounded-lg p-3 flex border-gray-300"
-                      type="email"
-                    />
+                  <label className="uppercase text-sm py-2">Email</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="email"
+                  />
                 </div>
                 <div className="flex flex-col py-2">
-                    <label className="uppercase text-sm py-2">Subject</label>
-                    <input
-                      className="border-2 rounded-lg p-3 flex border-gray-300"
-                      type="text"
-                    />
+                  <label className="uppercase text-sm py-2">Subject</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                  />
                 </div>
                 <div className="flex flex-col py-2">
-                    <label className="uppercase text-sm py-2">Message</label>
-                    <textarea className="border-2 rounded-lg p-3 border-gray-300" rows='10'></textarea>
+                  <label className="uppercase text-sm py-2">Message</label>
+                  <textarea
+                    className="border-2 rounded-lg p-3 border-gray-300"
+                    rows="10"
+                  ></textarea>
                 </div>
-                <button className="w-full p-4 text-gray-100 mt-4">Send Message</button>
+                <button className="w-full p-4 text-gray-100 mt-4">
+                  Send Message
+                </button>
               </form>
             </div>
           </div>
         </div>
         <div className="flex justify-center py-12">
-            <Link href='/'>
-                <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30}/>
-                </div>
-            </Link>
+          <Link href="/">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+              <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30} />
+            </div>
+          </Link>
         </div>
       </div>
     </div>
